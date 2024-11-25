281b6a36798fa57012bd4b8c76c5c89dd9027d61
JoaooPablo <joaoo@uninorte.edu.co>
2024-11-25 13:32:40 -0500

    Descripción breve de los cambios

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = 3000;
+const PORT = 80;
 
 // Middleware para servir archivos estáticos (CSS, imágenes, JS, etc.)
 app.use(express.static('public'));
@@ -15,7 +15,26 @@ app.get('/Arreglos-florales', (req, res) => {
     res.sendFile(__dirname + '/views/Arreglos-florales.html');
 });
 
+app.get('/Bouquets', (req, res) => {
+    res.sendFile(__dirname + '/views/Bouquets.html');
+});
 
+app.get('/Desayunos', (req, res) => {
+    res.sendFile(__dirname + '/views/Desayunos.html');
+});
+
+app.get('/Anchetas', (req, res) => {
+    res.sendFile(__dirname + '/views/Anchetas.html');
+});
+app.get('/Peluches', (req, res) => {
+    res.sendFile(__dirname + '/views/Peluches.html');
+});
+app.get('/Tazas', (req, res) => {
+    res.sendFile(__dirname + '/views/Tazas.html');
+});
+app.get('/Bolsos', (req, res) => {
+    res.sendFile(__dirname + '/views/Bolsos.html');
+});
 // Iniciar el servidor en el puerto 3000
 app.listen(PORT, () => {
     console.log(`Servidor escuchando en http://localhost:${PORT}`);
